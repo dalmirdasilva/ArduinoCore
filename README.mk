e98e15490b3bfff044c126b1682e032279a9a74f
Dalmir Silva <dalmirdasilva@gmail.com>
2015-08-11 16:54:31 -0300

    sync

---
+++ README.mk
@@ -0,0 +1,20 @@
+# ArduinoCore
+
+ArduinoCore is a Eclipse project based on the Arduino 1.6.5.
+
+## Setup
+
+- Download Eclipse IDE for Java Developers (Version: Luna Service Release 1a (4.4.1) Build id: 20150109-0600)
+You may not be able to find the exact versions that are used in this tutorial, just you the most updated versions.
+
+Java Runtime 5 or higher – Most likely you will already have this installed on your computer.
+Eclipse IDE for C/C++ Developers (~79 MB) – For this tutorial we’ll be using Build id: 20090920-1017.
+Arduino software – For this tutorial we will be using Arduino 0018.
+Eclipse AVR plugin – For this tutorial we will be using version 2.3.1
+WinAVR –  For this tutorial we will be using WinAVR-20100110
+Installation
+Install Java runtime. Most computers will already have Java installed as it is used for many other applications.
+Install Arduino software. The Arduino software comes with the Arduino library and source code examples that will be useful when developing your own projects.
+Install Eclipse. This should be pretty straight forward. You can install Eclipse in any directory you want. For this tutorial we installed it in C:\Dev\eclipse-cpp
+Install WinAVR, You can install WinAVR in any directory you want. For this tutorial we installed it in C:\Dev\WinAVR-20100110
+Install Eclipse AVR plugin. You will have to manually copy this “plugin” into the the Eclipse installation. Move the folder which begins with the name “de.innot.avreclipse” to the “dropins” folder located within the eclipse folder. Restart Eclipse if you have it already running. There should now be a new “AVR” toolbar button in your workspace.
